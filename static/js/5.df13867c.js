be570ac0f0a5b5cb026df835487c57e6589851c1
Daniel Kuroski <daniel.kuroski@gmail.com>
2018-12-13 18:14:28 -0200

    Update site [ci skip]

--- static/js/5.df13867c.js
+++ static/js/5.df13867c.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"./.docz/app/imports.js":function(n,o,t){"use strict";t.r(o),t.d(o,"imports",function(){return s});var s={"docs/buttons.mdx":function(){return Promise.all([t.e(0),t.e(2)]).then(t.bind(null,"./docs/buttons.mdx"))},"docs/typography.mdx":function(){return Promise.all([t.e(0),t.e(3)]).then(t.bind(null,"./docs/typography.mdx"))}}}},0,[0,2,3]]);
-//# sourceMappingURL=5.6c09455c48b26c945def.js.map
+//# sourceMappingURL=5.f24f69ba5b31ee160a62.js.map
