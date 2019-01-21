6c165ecbfe551d10af1af6e0df8316abcf91667b
Lari Maza <larissa.maza@gmail.com>
2019-01-21 15:47:30 -0200

    Update site [ci skip]

--- static/js/5.df13867c.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"./.docz/app/imports.js":function(n,o,t){"use strict";t.r(o),t.d(o,"imports",function(){return s});var s={"docs/buttons.mdx":function(){return Promise.all([t.e(0),t.e(2)]).then(t.bind(null,"./docs/buttons.mdx"))},"docs/typography.mdx":function(){return Promise.all([t.e(0),t.e(3)]).then(t.bind(null,"./docs/typography.mdx"))}}}},0,[0,2,3]]);
-//# sourceMappingURL=5.f24f69ba5b31ee160a62.js.map
