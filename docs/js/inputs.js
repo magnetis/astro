a61941bf92a8ffdaf2187db749268f2974a6bab3
Bruno Braga <bruno.chagas@magnetis.com.br>
2019-03-29 15:32:48 -0300

    Rebase with branch master

--- docs/js/inputs.js
+++ docs/js/inputs.js
@@ -1,35 +1,55 @@
 export const initFloatingLabel = () => {
   window.requestAnimationFrame(() => {
-    const inputs = document.querySelectorAll(".a-input");
+    const inputs = document.querySelectorAll('.a-input');
 
     const verifyValue = (input, label) => {
       if (input.value) {
-        label.classList.add("a-input--floating-label");
+        label.classList.add('a-input--floating-label');
       } else {
-        label.classList.remove("a-input--floating-label");
+        label.classList.remove('a-input--floating-label');
       }
     };
 
     inputs.forEach(input => {
       const currentInput = input.firstChild;
-      const currentLabel = input.querySelector("label");
+      const currentLabel = input.querySelector('label');
 
       verifyValue(currentInput, currentLabel);
 
-      currentInput.addEventListener("focus", () => {
+      currentInput.addEventListener('focus', () => {
         verifyValue(currentInput, currentLabel);
       });
 
-      currentInput.addEventListener("focusout", () => {
+      currentInput.addEventListener('focusout', () => {
         verifyValue(currentInput, currentLabel);
       });
     });
   });
 };
 
-const HandleFloatingLabel = () => {
+export const initMessagingInputEvent = () => {
+  window.requestAnimationFrame(() => {
+    const messagingInputs = document.querySelectorAll('.a-input--messaging');
+
+    messagingInputs.forEach(input => {
+      const currentMessagingInput = input.firstChild;
+      const currentButton = input.querySelector('button');
+
+      currentMessagingInput.addEventListener('input', event => {
+        if (event.target.value) {
+          currentButton.disabled = false;
+        } else {
+          currentButton.disabled = true;
+        }
+      });
+    });
+  });
+};
+
+const HandleInputEvents = () => {
   initFloatingLabel();
+  initMessagingInputEvent();
   return null;
 };
 
-export default HandleFloatingLabel;
+export default HandleInputEvents;
