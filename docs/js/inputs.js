8e365df7006fe52536097ec4be380d0bea3d93c2
Bruno Braga <bruno.chagas@magnetis.com.br>
2019-04-02 14:56:26 -0300

    Create messaging input handle

--- docs/js/inputs.js
+++ docs/js/inputs.js
@@ -29,8 +29,48 @@ export const initFloatingLabel = () => {
   });
 };
 
+export const initMessagingInputEvent = () => {
+  window.requestAnimationFrame(() => {
+    const messagingInputs = document.querySelectorAll('.a-input--messaging');
+
+    const handleFocusEvent = (input, button) => {
+      input.addEventListener('focus', event => {
+        if (!event.target.value) {
+          button.disabled = false;
+        }
+      });
+
+      input.addEventListener('focusout', event => {
+        if (!event.target.value) {
+          button.disabled = true;
+        } else {
+          button.disabled = false;
+        }
+      });
+    };
+
+    messagingInputs.forEach(input => {
+      const currentMessagingInput = input.firstChild;
+      const currentButton = input.querySelector('button');
+
+      if (currentMessagingInput.value) currentButton.disabled = false;
+
+      handleFocusEvent(currentMessagingInput, currentButton);
+
+      currentMessagingInput.addEventListener('input', event => {
+        if (event.target.value) {
+          handleFocusEvent(currentMessagingInput, currentButton);
+        } else {
+          handleFocusEvent(currentMessagingInput, currentButton);
+        }
+      });
+    });
+  });
+};
+
 const HandleInputEvents = () => {
   initFloatingLabel();
+  initMessagingInputEvent();
   return null;
 };
 
