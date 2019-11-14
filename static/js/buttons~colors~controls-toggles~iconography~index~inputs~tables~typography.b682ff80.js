319f42dbe859cefbc88341974b2ac9b3dfc11503
Lari Maza <larissa.maza@gmail.com>
2019-11-14 12:00:13 -0300

    Updates

---
+++ static/js/buttons~colors~controls-toggles~iconography~index~inputs~tables~typography.b682ff80.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"./src/index.css":function(n,s,w){}}]);
+//# sourceMappingURL=buttons~colors~controls-toggles~iconography~index~inputs~tables~typography.fe13ffac3b9b61bcbcdc.js.map
--- static/js/buttons~colors~controls-toggles~iconography~index~inputs~typography.418b733e.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"./src/index.css":function(n,s,w){}}]);
-//# sourceMappingURL=buttons~colors~controls-toggles~iconography~index~inputs~typography.af948bdf5788fd4db001.js.map
