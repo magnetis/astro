8aa72790ce07dcb088266f06fdb3cff17f20cfc6
Lucas <agtlucas@me.com>
2020-01-09 10:07:18 -0300

    Updates

--- static/js/buttons~colors~controls-toggles~iconography~index~inputs~tables~typography.176a8e8f.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"./src/index.css":function(n,s,w){}}]);
-//# sourceMappingURL=buttons~colors~controls-toggles~iconography~index~inputs~tables~typography.57268835c422ceeeaaac.js.map
---
+++ static/js/buttons~colors~controls-toggles~iconography~index~inputs~tables~typography.92ee6081.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"./src/index.css":function(n,s,w){}}]);
+//# sourceMappingURL=buttons~colors~controls-toggles~iconography~index~inputs~tables~typography.906a0551a695ca755732.js.map
